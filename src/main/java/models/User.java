4c56c020f9f4d3a2f479a4a1febf573bc5394bdc
aurban <anna.urban.1996@wp.pl>
2025-01-28 17:49:48 +0100

    Added @Builder

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,7 +1,12 @@
 package models;
 
+import lombok.Builder;
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
+@Builder
 public class User implements Serializable {
     public String socialTitle;
     public String firstName;
@@ -9,52 +14,5 @@ public class User implements Serializable {
     public String email;
     public String password;
     public String birthdate;
-
-    public void setSocialTitle(String socialTitle) {
-        this.socialTitle = socialTitle;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setBirthdate(String birthdate) {
-        this.birthdate = birthdate;
-    }
-
-    public String getSocialTitle() {
-        return socialTitle;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getBirthdate() {
-        return birthdate;
-    }
 }
+

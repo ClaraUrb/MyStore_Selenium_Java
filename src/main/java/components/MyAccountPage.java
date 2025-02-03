f37b04719690a4bb8bd10e9189d747e878d514b3
aurban <anna.urban.1996@wp.pl>
2025-02-03 18:25:50 +0100

    Assertion refactor

--- src/main/java/components/MyAccountPage.java
+++ src/main/java/components/MyAccountPage.java
@@ -1,15 +1,17 @@
 package components;
 
+import lombok.extern.slf4j.Slf4j;
 import models.User;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.asserts.SoftAssert;
 
+@Slf4j
 public class MyAccountPage {
+
     private WebDriver driver;
-    SoftAssert softAssert = new SoftAssert();
+    public String socialTitle;
 
     @FindBy(id = "identity-link")
     private WebElement accountInfo;
@@ -23,33 +25,20 @@ public class MyAccountPage {
         accountInfo.click();
     }
 
-    public void verifyPersonalInfo(User user) {
+    public User readUserData() {
         CreateAccountPage createAccountPage = new CreateAccountPage(driver);
-        boolean isMale = createAccountPage.mr.isSelected();
-        boolean isFemale = createAccountPage.mrs.isSelected();
-        if (user.getSocialTitle().equals("Mr")) {
-            softAssert.assertTrue(isMale);
-        } else if (user.getSocialTitle().equals("Ms")) {
-            softAssert.assertTrue(isFemale);
+        User.UserBuilder user = User.builder();
+        if (createAccountPage.mr.isSelected()) {
+            socialTitle = "Mr";
+        } else if (createAccountPage.mrs.isSelected()) {
+            socialTitle = "Mrs";
         }
-        softAssert.assertEquals(createAccountPage.firstName.getAttribute("value"), user.getFirstName());
-        softAssert.assertEquals(createAccountPage.lastName.getAttribute("value"), user.getLastName());
-        softAssert.assertEquals(createAccountPage.email.getAttribute("value"), user.getEmail());
-        softAssert.assertEquals(createAccountPage.birthdate.getAttribute("value"), user.getBirthdate());
-        testShowPasswordButtons();
-        softAssert.assertAll();
-    }
-
-    private void testShowPasswordButtons() {
-        CreateAccountPage createAccountPage = new CreateAccountPage(driver);
-
-        softAssert.assertEquals(createAccountPage.showPasswordButton.getText(), "SHOW");
-        softAssert.assertEquals(createAccountPage.showNewPasswordButton.getText(), "SHOW");
-
-        createAccountPage.showPasswordButton.click();
-        softAssert.assertEquals(createAccountPage.showPasswordButton.getText(),"HIDE");
-
-        createAccountPage.showNewPasswordButton.click();
-        softAssert.assertEquals(createAccountPage.showNewPasswordButton.getText(), "HIDE");
+        user
+                .socialTitle(socialTitle)
+                .firstName(createAccountPage.firstName.getAttribute("value"))
+                .lastName(createAccountPage.lastName.getAttribute("value"))
+                .email(createAccountPage.email.getAttribute("value"))
+                .birthdate(createAccountPage.birthdate.getAttribute("value"));
+        return user.build();
     }
 }
